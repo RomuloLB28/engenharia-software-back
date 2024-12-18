1d3f46a1e97472c57f72b756482b315ec7af6921
Romulo Lobato Bezerra <111526886+RomuloLB28@users.noreply.github.com>
2024-12-18 14:14:41 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,11 @@ import { UsuariosModule } from './usuarios/usuarios.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'process.env.DATABASE_HOST',
+      host: process.env.DATABASE_HOST,
       port: Number(process.env.DATABASE_PORT),
-      username: 'process.env.DATABASE_USER',
-      password: 'process.env.DATABASE_PASSWORD',
-      database: 'process.env.DATABASE_DBNAME',
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DBNAME,
       autoLoadEntities: true,
       synchronize: false,
     }),
