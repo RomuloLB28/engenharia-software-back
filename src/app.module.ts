6bbdaaa6db3c3a16c236f41a94e6423f2de9455c
Romulo Lobato Bezerra <111526886+RomuloLB28@users.noreply.github.com>
2024-12-18 15:11:12 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,11 @@ import { UsuariosModule } from './usuarios/usuarios.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host:mysql-d293004-romulo-lobato.j.aivenclound.com,
-      port:Number(28237),
-      username:avnadmin,
-      password:AVNS_1gzPo43od3YQvNHTZMi,
-      database:defaultdb,
+      host:process.env.DATABASE_HOST,
+      port:Number(process.env.DATABASE_PORT),
+      username:process.env.DATABASE_USER,
+      password:process.env.DATABASE_PASSWORD,
+      database:process.env.DATABASE_DBNAME,
       autoLoadEntities: true,
       synchronize: false,
     }),
