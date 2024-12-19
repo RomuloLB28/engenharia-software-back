216a2b94f1cc11ce8ae873406d4f1ec70f778239
Romulo Lobato Bezerra <111526886+RomuloLB28@users.noreply.github.com>
2024-12-19 11:14:45 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   // Habilitar CORS com configurações completas
   app.enableCors({
-    origin: 'http://localhost:3000', // Permite apenas o frontend React loca
+    origin: 'https://engenharia-software-front.vercel.app', // Permite apenas o frontend React loca
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS', // Métodos HTTP permitidos
     allowedHeaders: 'Content-Type, Authorization', // Headers permitidos
     credentials: true, // Permite cookies e credenciais
